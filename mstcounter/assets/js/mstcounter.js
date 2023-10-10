716b15e543d15b9cc64d1560fe22032082317d6a
wolkanca <wolkanca@gmail.com>
2023-10-10 14:46:19 +0300

    clean coding. remove bugs. mstcounter.js
    clean coding. remove bugs.

--- mstcounter/assets/js/mstcounter.js
+++ mstcounter/assets/js/mstcounter.js
@@ -1,7 +1,7 @@
 // MST SERP Counter - SEO Free SERP Checker Tool 
 // https://chrome.google.com/webstore/detail/mst-serp-counter-seo-free/dgpekiojagcbjhgfpmmngnkchekcfifn
 // mstcounter.js
-// 👍 wolkanca.com
+// 👍 https://wolkanca.com/chrome-serp-counter-uzantisi/
 
 function ok() {
 	const tags = [...document.querySelectorAll('.ok .serpcounter')];
@@ -15,34 +15,24 @@ function ok() {
 	});
 }
 
+/*
+const search = document.querySelectorAll('#search')
+const botstuff = document.querySelectorAll('#botstuff')
 const yuRUbf = document.getElementsByClassName('yuRUbf');
-const MjjYud = document.getElementsByClassName('MjjYud');
+*/
+
+const results = document.getElementsByClassName('MjjYud');
 
 function mstSerpMain() {
 	let countDisplay = 1;
-	for (let countActual = 0; countActual < MjjYud.length; countActual++, countDisplay++) {
-
-		// Skip if URL is invisible (PAA box, other search features)
-		let height = getComputedStyle(yuRUbf[countActual].querySelector('a h3')).height;
-
-		if (height == "auto") {
-			let height = 0;
-		}
-		if (height < 20) {
-			countDisplay--;
-			continue;
-		}
-
+	for (let countActual = 0; countActual < results.length; countActual++, countDisplay++) {
 		let count = countDisplay;
-		let parentE = MjjYud[countActual];
-
+		let parentE = results[countActual];
 		let counter = document.createElement('div');
 		counter.className = 'serpcounter';
 		counter.prepend(count);
-
 		parentE.prepend(counter);
 		parentE.classList.add("ok");
-
 	}
 	return ok();
 }
